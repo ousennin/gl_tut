e8aa4a59555fd4790b05afc6541a1803d8f945db
ousennin <cob-rabota@yandex.ru>
2023-01-16 08:46:43 +0200

    wip commit

---
+++ app/src/main/java/anonymous/gl/utils/geometry/Cylinder.kt
@@ -0,0 +1,9 @@
+package anonymous.gl.utils.geometry
+
+import android.graphics.Point
+
+class Cylinder(
+    val center: Point,
+    val radius: Float,
+    val height: Float,
+)
---
+++ app/src/main/java/anonymous/gl/utils/geometry/ObjectBuilder.kt
@@ -0,0 +1,9 @@
+package anonymous.gl.utils.geometry
+
+class ObjectBuilder private constructor() {
+    private companion object {
+        const val FLOATS_PER_VERTEX = 3
+    }
+    private var offset: Int = 0
+//    private val vertexData: FloatArray
+}
